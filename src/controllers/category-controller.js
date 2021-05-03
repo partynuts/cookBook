053ab2039b859b2ea208f48a1511aacb4d771981
proghany <proghany@ebay.com>
2021-05-03 20:59:30 +0200

    add endpoint for create category in controller

--- src/controllers/category-controller.js
+++ src/controllers/category-controller.js
@@ -1,6 +1,13 @@
 const { Router } = require("express");
 const controller = Router();
+const { createCategory } = require('../models.category-model');
 
 controller.post('/api/categories', async (req, res) => {
-  console.log("CREATING CATEGORY")
+  console.log("CREATING CATEGORY", req.body);
+  const foundCategory = await getCategoryByTitle(req.body.categoryTitle);
+  if (foundCategory) {
+    return res.status(400).json({ errorMsg: 'Diese Kategorie gibt es bereits.' });
+  }
+  const newCategory = await createCategory(req.body.categoryTitle, req.body.categoryDescription);
+  return res.status(201).json(newCategory);
 });
